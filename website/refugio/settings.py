fceefca5450a105057bd5c8de7292705dd69147a
jessmelo <50722304+jessmelo@users.noreply.github.com>
2021-08-22 01:28:12 -0300

    adicionado template de pagina inicial

--- website/refugio/settings.py
+++ website/refugio/settings.py
@@ -67,6 +67,15 @@ TEMPLATES = [
     },
 ]
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'refugio/templates/static'
+]
+
 WSGI_APPLICATION = 'refugio.wsgi.application'
 
 
